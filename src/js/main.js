61ab3aaa650f650ef6e9f03509a0c3b2ec813853
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-03-24 13:46:42 +0300

    Сделано меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,16 @@
-var accItem = document.getElementsByClassName('questions-answer');
-var accHD = document.getElementsByClassName('questions-answer__title');
-var popupName = ('.popup-answer');
-var link = ('.button');
-
-for (i = 0; i < accHD.length; i++) {
-    accHD[i].addEventListener('click', toggleItem, false);
-}
-function toggleItem() {
-    var itemClass = this.parentNode.className;
-    for (i = 0; i < accItem.length; i++) {
-        accItem[i].className = 'questions-answer close';
-    }
-    if (itemClass == 'questions-answer close') {
-        this.parentNode.className = 'questions-answer open';
-    }
-}
-/* modal popup */
-$(document).ready(function($) {
-  $('.button').on('click', function() {
-    $('.modal-answer').text($(this).attr('data-popup'));
-    $('.modal').css("display", "flex").hide().fadeIn();
-    return false;
-  });	
-	
-  $('.modal-close').click(function() {
-    $(this).parents('.modal').fadeOut();
-    return false;
-  });		
+$(document).ready(function () {
+  
+  var scrolling = $(".hero-navbar");
  
-  $(document).keydown(function(e) {
-    if (e.keyCode === 27) {
-      e.stopPropagation();
-      $('.modal').fadeOut();
+  $(window).scroll(function () {
+    if ((document.documentElement.clientWidth < 768)) {
+      scrolling.removeClass("scrolling").addClass("loading");
+    }
+    else if ($(this).scrollTop() >= 70 && scrolling.hasClass("loading")) {
+      scrolling.removeClass("loading").addClass("scrolling");
+    } else if ($(this).scrollTop() <= 70 && scrolling.hasClass("scrolling")) {
+      scrolling.removeClass("scrolling").addClass("loading");
     }
   });
-	
-  $('.modal').click(function(e) {
-    if ($(e.target).closest('.modal-block').length == 0) {
-      $(this).fadeOut();					
-    }
-  });
-});
 
+});
